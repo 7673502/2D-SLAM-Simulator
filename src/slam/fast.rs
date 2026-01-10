b1715ca0c9a0e4d5dea85124871b2d472fcffa20
Pramod Anandarao <panandarao@wisc.edu>
2026-01-10 12:51:35 -0600

    implemented FastSLAM prediction step

--- src/slam/fast.rs
+++ src/slam/fast.rs
@@ -4,6 +4,7 @@ use nalgebra::{Matrix2, Vector2};
 use crate::slam::Slam;
 use crate::simulation::Observation;
 use crate::config::Config;
+use crate::utils::sample_normal;
 
 #[derive(Clone)]
 pub struct LandmarkEstimate {
@@ -46,8 +47,21 @@ impl FastSlam {
 }
 
 impl Slam for FastSlam {
-    fn predict(&mut self, linear_vel: f32, angular_vel: f32, delta_time: f32, cfg: &Config) {
-        todo!()
+    fn predict(&mut self, linear_velocity: f32, angular_velocity: f32, delta_time: f32, cfg: &Config) {
+        for particle in &mut self.particles {
+            let noisy_linear_velocity = linear_velocity + sample_normal(0.0, (cfg.est_stdev_linear * linear_velocity.abs()).max(0.01));
+            let noisy_angular_velocity  = angular_velocity + sample_normal(0.0, (cfg.est_stdev_linear * linear_velocity.abs()).max(0.01));
+
+            let theta_half = particle.theta + 0.5 * noisy_angular_velocity * delta_time;
+        
+            // update position estimate
+            particle.x += noisy_linear_velocity * delta_time * theta_half.cos();
+            particle.y += noisy_linear_velocity * delta_time * theta_half.sin();
+            particle.theta += noisy_angular_velocity * delta_time;
+
+            // normalize angle to (-PI, PI]
+            particle.theta = f32::atan2(particle.theta.sin(), particle.theta.cos());
+        }
     }
 
     fn update(&mut self, observations: &[Observation], cfg: &Config) {
