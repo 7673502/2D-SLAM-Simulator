6717ac64c18cdf9404a2633d514e2fca146efaf1
Pramod Anandarao <panandarao@wisc.edu>
2026-01-13 22:52:18 -0600

    added 😶

--- src/ui.rs
+++ src/ui.rs
@@ -82,7 +82,8 @@ pub fn draw_robot_shadow(x: f32, y: f32, radius: f32) {
 
 pub fn draw_robot(x: f32, y: f32, theta: f32, radius: f32, fill_color: Color, outline_color: Color) {
     draw_circle(x, y, radius, fill_color);
-    draw_circle(x + 0.5 * radius * theta.cos(), y + 0.5 * radius * theta.sin(), radius / 6.0, outline_color);
+    draw_circle(x + 0.5 * radius * (theta - 0.8).cos(), y + 0.5 * radius * (theta - 0.8).sin(), radius / 6.0, outline_color);
+    draw_circle(x + 0.5 * radius * (theta + 0.8).cos(), y + 0.5 * radius * (theta + 0.8).sin(), radius / 6.0, outline_color);
 }
 
 pub fn draw_slam_state(slam: &dyn Slam, radius: f32) {
